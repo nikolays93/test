cce1db2522c235170c111da1c00c82c565715610
nikolays93 <nikolays93@ya.ru>
2019-08-11 22:05:25 +0400

    Написал первый тест

--- php7/test/GildedRoseTest.php
+++ php7/test/GildedRoseTest.php
@@ -2,11 +2,54 @@
 
 namespace App;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    public function testFoo() {
-        $items      = [new Item("foo", 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertEquals("fixme", $items[0]->name);
+use App\Product\DefaultProduct;
+
+class GildedRoseTest extends \PHPUnit\Framework\TestCase
+{
+    public function defaultItemsDataClassNameProvider()
+    {
+        $itemName      = 'UndefinedName';
+        $exceptedClass = DefaultProduct::class;
+
+        return array(
+            array($itemName, $exceptedClass, 1, 1, 0, 0),
+            array($itemName, $exceptedClass, 0, 10, -1, 8),
+            array($itemName, $exceptedClass, -5, 10, -6, 8),
+            array($itemName, $exceptedClass, 1, 0, 0, 0),
+            array($itemName, $exceptedClass, -5, 0, -6, 0),
+        );
+    }
+
+    /**
+     * @dataProvider defaultItemsDataClassNameProvider
+     *
+     * @param string $name
+     * @param string $className
+     * @param float $sellIn
+     * @param float $quality
+     * @param float $exceptionSellIn
+     * @param float $exceptionQuality
+     */
+    public function testUpdate(
+        string $name,
+        string $className,
+        float $sellIn,
+        float $quality,
+        float $exceptionSellIn,
+        float $exceptionQuality
+    ) {
+        $exampleItem = new Item($name, $sellIn, $quality);
+        $gildedRose  = new GildedRose();
+        $gildedRose
+            ->add($exampleItem)
+            ->build()
+            ->update();
+
+        // get once (first) item
+        $item = $gildedRose->get();
+
+        $this->assertEquals($className, get_class($item));
+        $this->assertEquals($exceptionSellIn, $item->getSellIn());
+        $this->assertEquals($exceptionQuality, $item->getQuality());
     }
 }
